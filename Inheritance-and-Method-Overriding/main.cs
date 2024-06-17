d91d10c5ffb36896ca4fc2d5c581a6d3b0deecf6
Adenutsi Andy Eleos <102770998+leoisqualified@users.noreply.github.com>
2024-06-17 08:06:48 +0000

    Add files via upload

---
+++ Inheritance-and-Method-Overriding/main.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace InheritanceAndMethodOverriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
