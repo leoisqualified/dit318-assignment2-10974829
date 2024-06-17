6877923a1c70356e5dc229588de445ec7649a7ef
Adenutsi Andy Eleos <102770998+leoisqualified@users.noreply.github.com>
2024-06-17 11:21:07 +0000

    Add files via upload

---
+++ Interfaces/Bicycle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Interfaces
+{
+    // Create the class Bicycle that implements IMovable
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+}
---
+++ Interfaces/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+
+namespace Interfaces
+{
+    // Create the class Car that implements IMovable
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+}
+
