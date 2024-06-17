c9366c9d56601903bb72a93ea1c9bbc17083befc
Adenutsi Andy Eleos <102770998+leoisqualified@users.noreply.github.com>
2024-06-17 11:13:25 +0000

    Add files via upload

---
+++ Inheritance and Method Overriding/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InheritanceAndMethodOverriding
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+}
+
---
+++ Inheritance and Method Overriding/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InheritanceAndMethodOverriding
+{
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+}
+
