613f340f626f567d3c537b7f290d919d976d9967
Adenutsi Andy Eleos <102770998+leoisqualified@users.noreply.github.com>
2024-06-17 11:05:45 +0000

    Add files via upload

---
+++ Dcit Assignment 2 10974829/Dog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace InheritanceAndMethodOverriding
+{
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+}
+
